fe9b0f33dd2424def8e90c83e73809fd8323adcc
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-14 21:55:45 +0100

    Add files via upload

--- binarys.py
+++ binarys.py
@@ -3,6 +3,12 @@ import math
 def openw(names:str):
     return open(names,"bw")
 
+def openr(names:str):
+    return open(names,"br")
+
+def openr(names:str):
+    return open(names,"ba")
+
 
 def closew(f1):
     f1.close()
@@ -15,15 +21,34 @@ def writesS(f1,s:str):
 def writesf(f1,f):
     f1.write(struct.pack('<f',f))
 
-def writesi(f1,i):
+def writesi(f1,i:int):
     f1.write(struct.pack('<i',i))
 
-print("\033c\033[47;30m\nwrite")
+def readsS(f1,s:str,i:int):
+    r=f1.read(i)
+    a=r.decode()
 
+def readsf(f1):
+    r=f1.read(4)
+    ff=struct.unpack('<f',r)
+    return ff
+
+def writesi(f1):
+    r=f1.write(4)
+    ii=struct.pack('<i',r)
+    return ii
+
+print("\033c\033[47;30m\nwrite")
+r="hello\n"
+rr=len(r)
 f1=openw("my.bin")
 writesS(f1,"hello\n")
-writesi(f1,int(128))
+writesi(f1,128)
 writesf(f1,math.pi)
+f1=openr("my.bin")
+print(readsS(f1,rr))
+print(readsi(f1))
+print(readsf(f1))
 
 closew(f1)
 
