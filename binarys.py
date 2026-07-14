e081957931a1c357ba8fb333004490bca3c5cf4e
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-14 22:06:53 +0100

    Add files via upload

--- binarys.py
+++ binarys.py
@@ -6,7 +6,7 @@ def openw(names:str):
 def openr(names:str):
     return open(names,"br")
 
-def openr(names:str):
+def opena(names:str):
     return open(names,"ba")
 
 
@@ -24,19 +24,20 @@ def writesf(f1,f):
 def writesi(f1,i:int):
     f1.write(struct.pack('<i',i))
 
-def readsS(f1,s:str,i:int):
+def readsS(f1,i:int):
     r=f1.read(i)
     a=r.decode()
+    return a
 
 def readsf(f1):
     r=f1.read(4)
     ff=struct.unpack('<f',r)
-    return ff
+    return ff[0]
 
-def writesi(f1):
-    r=f1.write(4)
-    ii=struct.pack('<i',r)
-    return ii
+def readsi(f1):
+    r=f1.read(4)
+    ii=struct.unpack('<i',r)
+    return ii[0]
 
 print("\033c\033[47;30m\nwrite")
 r="hello\n"
@@ -45,6 +46,8 @@ f1=openw("my.bin")
 writesS(f1,"hello\n")
 writesi(f1,128)
 writesf(f1,math.pi)
+closew(f1)
+
 f1=openr("my.bin")
 print(readsS(f1,rr))
 print(readsi(f1))
